aff59bdcd3352c48a5d89fedeb0f47640961086d
threatt-zm <10990825@live.mercer.edu>
2025-08-08 20:16:18 -0400

    created repl to print first word from input

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,26 @@
+
+import { rawListeners } from "node:process";
+import { createInterface } from "node:readline";
+import Stream from "node:stream";
+
 export function cleanInput(input:string):string[] {
     return input.trim().split(" ").filter(word => word != "").map(word => word.toLowerCase());
 }
+
+export function startREPL():void {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > ",
+    });
+
+    rl.prompt();
+    rl.on("line", (input) => {
+        const inputs = cleanInput(input);
+        if(inputs.length > 0) {
+            console.log(`Your command was: ${inputs[0]}`);
+        }
+        rl.prompt();
+    });
+}
+
